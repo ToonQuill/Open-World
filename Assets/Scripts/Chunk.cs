57767428bea2bde4754ffc38678c743f6bc3d988
Tyler Warr <tylerw1997@hotmail.co.uk>
2020-02-09 13:37:50 +0000

    Week 2

--- Assets/Scripts/Chunk.cs
+++ Assets/Scripts/Chunk.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System.IO;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -8,8 +9,18 @@ public class Chunk : MonoBehaviour
     private Vector3 playerPos;
 
     float chunkDistToPlayer;
+    float unloadedChunkDistToPlayer;
     float npcDistToPlayer;
     float sceneDistToPlayer;
+
+    public GameObject prefab;
+
+    GameObject clone;
+
+    private int numChunks = 81;
+    public int currentCycle = 1;
+
+    private string json;
     // Start is called before the first frame update
     void Start()
     {
@@ -25,16 +36,44 @@ public class Chunk : MonoBehaviour
             foreach (Transform child in transform)
             {
                 chunkDistToPlayer = Vector3.Distance(playerPos, child.transform.position);
-                if (chunkDistToPlayer < 100)
+                if (chunkDistToPlayer > 100)
                 {
-                    child.gameObject.SetActive(true);
-                }
-                else
-                {
-                    child.gameObject.SetActive(false);
+                    //if out of range, save gameobject name and position, then destroy - SAVE
+                    SaveObjectData saveObject = new SaveObjectData
+                    {
+                        gameObjectName = child.transform.name,
+                        position = new Vector3(child.transform.position.x, child.transform.position.y, child.transform.position.z),
+                    };
+                    json = JsonUtility.ToJson(saveObject, true);
+                    File.WriteAllText(Application.dataPath + "/WorldData/" + child.transform.name + ".txt", json);
+                    Destroy(child.gameObject);
                 }
             }
         }
+        //check if player is in range of any saved positions - LOAD
+        if (File.Exists(Application.dataPath + "/WorldData/Zone " + currentCycle + ".txt"))
+        {
+            string findObjectString = File.ReadAllText(Application.dataPath + "/WorldData/Zone " + currentCycle + ".txt");
+            SaveObjectData saveObject = JsonUtility.FromJson<SaveObjectData>(findObjectString);
+             unloadedChunkDistToPlayer = Vector3.Distance(playerPos, saveObject.position);
+            if (unloadedChunkDistToPlayer < 100)
+            {
+                clone = Instantiate(prefab, saveObject.position, Quaternion.identity);
+                clone.gameObject.name = "Zone " + currentCycle;
+                clone.transform.parent = this.transform.parent;
+                File.Delete(Application.dataPath + "/WorldData/Zone " + currentCycle + ".txt");
+            }
+        }
+        currentCycle++;
+        if (currentCycle > 82)
+        {
+            currentCycle = 1;
+        }
+
+
+
+
+
         if (gameObject.tag == "NPC")
         {
             npcDistToPlayer = Vector3.Distance(playerPos, transform.position);
@@ -51,6 +90,25 @@ public class Chunk : MonoBehaviour
         {
 
         }
+    }
+
+    [System.Serializable]
+    public class WorldData
+    {
+        public SaveObjectData[] worldData;
 
     }
+
+    public class SaveObjectData
+    {
+        public Vector3 position;
+        public string gameObjectName;
+    }
+
+    public class LoadObject
+    {
+
+    }
+
+
 }
